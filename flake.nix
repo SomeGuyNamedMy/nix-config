53798a0b5c62c8eb35328c0bdea3546aec1c4555
SomeGuyNamedMy <mfdear444@gmail.com>
2022-11-08 17:19:40 -0500

    add sub

--- flake.nix
+++ flake.nix
@@ -25,10 +25,12 @@
                     nix.settings.trusted-public-keys = [
                         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
                         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
                     ];
                     nix.settings.substituters = [
                         "https://cache.iog.io"
                         "https://nix-community.cachix.org"
+                        "https://hyprland.cachix.org"
                     ];
                 }
     ];
